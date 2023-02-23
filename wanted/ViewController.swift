357f8a7affff4f919f0a4a10e684565b25b7689e
황경원 <gof9325@gmail.com>
2023-02-23 10:06:36 +0900

    Initial Commit

---
+++ wanted/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted
+//
+//  Created by 황경원 on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
